a37744242cc57ee681ca9f8b44848f0fbddd90fd
meeen <31080159+meeen@users.noreply.github.com>
2021-01-25 22:27:46 +0100

    some settings were wrong

--- testrest/settings.py
+++ testrest/settings.py
@@ -79,23 +79,13 @@ WSGI_APPLICATION = 'testrest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mlproducts',
-        'USER': 'postgres',
-        'PASSWORD': 'menodb1',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
